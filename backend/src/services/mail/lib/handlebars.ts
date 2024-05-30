07f843d949efd33b6409961ed3e984dd875ab932
Chris YESSO <yessochrisa@gmail.com>
2024-05-30 16:43:42 +0000

    Fixing mail TLS

--- backend/src/services/mail/lib/handlebars.ts
+++ backend/src/services/mail/lib/handlebars.ts
@@ -3,33 +3,37 @@ import nodemailer from "nodemailer";
 import hbs from "nodemailer-express-handlebars";
 import { ExpressHandlebars } from "express-handlebars";
 import {
-    SMTP_ADDRESS,
-    SMTP_EMAIL,
-    SMTP_PASSWORD,
-    SMTP_PORT,
+	SMTP_ADDRESS,
+	SMTP_EMAIL,
+	SMTP_PASSWORD,
+	SMTP_PORT,
 } from "../../../../config";
 
 const transporter = nodemailer.createTransport({
-    host: SMTP_ADDRESS,
-    port: SMTP_PORT,
-    secure: false, // Use `true` for port 465, `false` for all other ports
-    auth: {
-        user: SMTP_EMAIL,
-        pass: SMTP_PASSWORD,
-    },
+	host: SMTP_ADDRESS,
+	port: SMTP_PORT,
+	secure: false,
+	tls: {
+		rejectUnauthorized: false,
+		secureProtocol: "TLSv1_method",
+	},
+	auth: {
+		user: SMTP_EMAIL,
+		pass: SMTP_PASSWORD,
+	},
 });
 
 transporter.use(
-    "compile",
-    hbs({
-        viewEngine: {
-            extname: ".hbs",
-            partialsDir: path.resolve("./src/services/mail/views/"),
-            defaultLayout: false,
-        },
-        viewPath: path.resolve("./src/services/mail/views"),
-        extName: ".hbs",
-    })
+	"compile",
+	hbs({
+		viewEngine: {
+			extname: ".hbs",
+			partialsDir: path.resolve("./src/services/mail/views/"),
+			defaultLayout: false,
+		},
+		viewPath: path.resolve("./src/services/mail/views"),
+		extName: ".hbs",
+	})
 );
 
 export default transporter;
