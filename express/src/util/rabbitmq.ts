1ed248a05708db2a04ebcbb80f0a7015efb22d6e
Chris YESSO <yessochrisa@gmail.com>
2024-06-04 11:30:32 +0000

    Transient producer connections

--- express/src/util/rabbitmq.ts
+++ express/src/util/rabbitmq.ts
@@ -1,16 +1,31 @@
 import amqp from "amqplib";
 import config from "../../config";
 
+var channel: any;
+var connection: any;
+
 export const connectToQueue = async () => {
-	const connection = await amqp.connect(config.rabbitmqUrl);
-	const channel = await connection.createChannel();
-	return channel;
+	try {
+		connection = await amqp.connect(config.rabbitmqUrl);
+		channel = await connection.createChannel();
+		return channel;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const publishToQueue = async (queue: string, message: object) => {
-	const channel = await connectToQueue();
-	channel.assertQueue(queue, {
-		durable: true,
-	});
-	channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+	try {
+		channel.assertQueue(queue, {
+			durable: true,
+		});
+		channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+
+		setTimeout(() => {
+			channel.close();
+			connection.close();
+		}, 500);
+	} catch (error) {
+		console.error(error);
+	}
 };
