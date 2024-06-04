772e639264ccca8ad53cfedb99075cb026d47a56
Chris YESSO <yessochrisa@gmail.com>
2024-06-04 11:37:58 +0000

    Fixing connections

--- express/src/util/rabbitmq.ts
+++ express/src/util/rabbitmq.ts
@@ -1,21 +1,11 @@
 import amqp from "amqplib";
 import config from "../../config";
 
-var channel: any;
-var connection: any;
-
-export const connectToQueue = async () => {
-	try {
-		connection = await amqp.connect(config.rabbitmqUrl);
-		channel = await connection.createChannel();
-		return channel;
-	} catch (error) {
-		console.error(error);
-	}
-};
-
 export const publishToQueue = async (queue: string, message: object) => {
 	try {
+		const connection = await amqp.connect(config.rabbitmqUrl);
+		const channel = await connection.createChannel();
+
 		channel.assertQueue(queue, {
 			durable: true,
 		});
