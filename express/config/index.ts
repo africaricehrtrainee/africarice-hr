5ad35457d2f12545766c6a3ebc0560f87c781e72
Chris Yesso <yessochrisa@gmail.com>
2024-06-21 11:25:00 +0000

    SAML ep.6

--- express/config/index.ts
+++ express/config/index.ts
@@ -2,15 +2,13 @@ import dotenv from "dotenv";
 import fs from "fs";
 dotenv.config();
 
-var uwIdpCert = fs.readFileSync("../certs/saml.pem", "utf-8");
-
 export default {
 	rabbitmqUrl: process.env.RABBITMQ_URL as string,
 	publicApi: process.env.PUBLIC_ADDRESS as string,
 	saml: {
 		entryPoint: process.env.SAML_ENTRY_POINT as string,
 		issuer: "passport-saml" as string,
-		idpCert: uwIdpCert,
+		idpCert: process.env.SAML_CERT as string,
 		callbackUrl: process.env.SAML_CALLBACK_URL as string,
 	},
 };
