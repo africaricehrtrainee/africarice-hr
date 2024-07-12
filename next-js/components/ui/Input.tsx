6fa2595880b16fa7898e20bcd214f377f70f7d4b
Chris Yesso <yessochrisa@gmail.com>
2024-07-12 10:40:55 +0000

    Fixed docker start config for dev env

--- next-js/components/ui/Input.tsx
+++ next-js/components/ui/Input.tsx
@@ -10,7 +10,7 @@ export function Input(props: InputProps) {
             className={twMerge(
                 "w-full p-2 px-3 mt-1 text-xs outline-none border disabled:bg-zinc-50  rounded-md border-zinc-200 hover:border-zinc-500 transition-all",
                 "focus:border-gray-300 focus:outline-gray-100",
-                props.required && !props.value && "border-red-300",
+                !props.disabled && props.required && !props.value && "border-red-300",
                 props.className
             )}
         ></input>
